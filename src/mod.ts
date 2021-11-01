d434e92a35cb6b5dff2a51bad7d77ed33401d129
Huan (李卓桓) <zixia@zixia.net>
2021-11-01 13:54:23 +0800

    add WrapAsyncError

--- src/mod.ts
+++ src/mod.ts
@@ -5,12 +5,20 @@ import {
   isGrpcStatus,
   isGError,
   GError,
-}                   from './gerror.js'
+}                   from './gerror/gerror.js'
+import {
+  wrapAsyncError,
+  WrapAsync,
+}                   from './wrap-async-error/wrap-async-error.js'
 
+export type {
+  WrapAsync,
+}
 export {
   isEcmaError,
   isGrpcStatus,
   isGError,
+  wrapAsyncError,
   GError,
   VERSION,
 }
---
+++ src/wrap-async-error/wrap-async-error.ts
@@ -0,0 +1,59 @@
+/**
+ * Wrap promise in sync way (catch error by emitting it)
+ *  1. convert a async callback function to be sync function
+ *    by catcing any errors and emit them to error event
+ *  2. wrap a Promise by catcing any errors and emit them to error event
+ */
+interface WrapAsync {
+
+  (promise: Promise<any>): void
+
+  <T extends (...args: any[]) => Promise<any>> (
+    asyncFunction: T,
+  ): (...args: Parameters<T>) => void
+
+}
+
+type OnErrorCallback = (error: any) => void
+
+const wrapAsyncError: (onError: OnErrorCallback) => WrapAsync = onError => <T extends (...args: any[]) => any | Promise<any>> (
+  asyncStaff: T | Promise<any>,
+) => {
+  /**
+   * 1. Promise
+   */
+  if (asyncStaff instanceof Promise) {
+    asyncStaff
+      .then(_ => _)
+      .catch(e => onError(e))
+    /**
+     * Huan(202110) FIXME:
+     *  The same code works inside the Wechaty/Puppet class
+     *  but here we  have to use `as any` to bypass the type check
+     */
+    return undefined as any
+  }
+
+  /**
+   * 2. Function
+   */
+  return function (this: any, ...args: Parameters<T>): void {
+    try {
+      const ret = asyncStaff.apply(this, args)
+      if (ret instanceof Promise) {
+        ret.catch(onError)
+      } else {
+        void ret  // <- Huan(202110): to see what type it is
+      }
+    } catch (e) {
+      onError(e)
+    }
+  }
+}
+
+export type {
+  WrapAsync,
+}
+export {
+  wrapAsyncError,
+}
